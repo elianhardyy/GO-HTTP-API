879db928e7dcaaa0aa095da657590bf233530aa8
elianhardyy <elianhardi805@gmail.com>
2024-10-16 06:14:24 +0700

    update router

--- routes/user_route.go
+++ routes/user_route.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 	"server/controllers"
 	"server/middlewares"
-)
 
+	"github.com/gorilla/mux"
+)
+var R = mux.NewRouter()
 func UserRoutes(api controllers.UserController) {
-	http.HandleFunc("/create",api.Register)
-	http.HandleFunc("/login",api.Login)
-	http.Handle("/userme",middlewares.ProtectedHandler(http.HandlerFunc(api.UserMe)))
-	http.Handle("/user",middlewares.ProtectedHandler(http.HandlerFunc(api.SingleUser)))
+	R.HandleFunc("/create",api.Register).Methods("POST")
+	R.HandleFunc("/login",api.Login).Methods("POST")
+	R.Handle("/userme",middlewares.ProtectedHandler(http.HandlerFunc(api.UserMe))).Methods("GET")
+	R.Handle("/user",middlewares.ProtectedHandler(http.HandlerFunc(api.SingleUser))).Methods("GET")
 }
